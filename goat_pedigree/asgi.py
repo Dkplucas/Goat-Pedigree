98aa6d868aa020cb9c3ecf5e597660079343b742
lu <dossoukponganfleming@gmail.com>
2025-01-24 14:10:54 +0900

    version 0.0.5

--- goat_pedigree/asgi.py
+++ goat_pedigree/asgi.py
@@ -1,16 +1,28 @@
-"""
-ASGI config for goat_pedigree project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from asgiref.compatibility import guarantee_single_callable
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goat_pedigree.settings')
 
-application = get_asgi_application()
+# Wrap the ASGI application with lifespan middleware
+class LifespanMiddleware:
+    def __init__(self, app):
+        self.app = guarantee_single_callable(app)
+
+    async def __call__(self, scope, receive, send):
+        if scope["type"] == "lifespan":
+            while True:
+                message = await receive()
+                if message["type"] == "lifespan.startup":
+                    await send({"type": "lifespan.startup.complete"})
+                elif message["type"] == "lifespan.shutdown":
+                    await send({"type": "lifespan.shutdown.complete"})
+                    return
+        else:
+            await self.app(scope, receive, send)
+
+# Get the Django ASGI application
+django_application = get_asgi_application()
+
+# Wrap it with the lifespan middleware
+application = LifespanMiddleware(django_application)
