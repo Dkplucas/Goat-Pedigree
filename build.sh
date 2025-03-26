9d2decd61c2258eef2fe042b03bc7bb02536a8fc
lu <dossoukponganfleming@gmail.com>
2025-03-26 20:14:14 +0900

    version 0.3.1

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
+# Add these lines at the top
+pip install --upgrade pip setuptools wheel
+pip install PyYAML==5.4.1
+
 # Exit on error
 set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --noinput
 python manage.py migrate
