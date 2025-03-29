246cb0ce8dce24f34311aec12037fe423dc36133
lu <dossoukponganfleming@gmail.com>
2025-03-29 16:30:17 +0900

    version 0.4.6

--- goat_project/settings.py
+++ goat_project/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-ALLOWED_HOSTS = ['goatpedigree.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['goatpedigree.onrender.com']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
@@ -167,17 +167,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Whitenoise configuration
-WHITENOISE_MANIFEST_STRICT = False  # Ignore missing files
-WHITENOISE_ALLOW_ALL_ORIGINS = True
-
 if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-else:
-    STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
 # Media files configuration
 MEDIA_URL = '/media/'
