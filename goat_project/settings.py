893f9270aa3cd6c4c12baac0d8aaf4fbdfb17f15
lu <dossoukponganfleming@gmail.com>
2025-03-29 20:56:29 +0900

    version 0.5.2

--- goat_project/settings.py
+++ goat_project/settings.py
@@ -122,9 +122,9 @@ WSGI_APPLICATION = 'goat_project.wsgi.application'
 # Database
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL', default='sqlite:///' + str(BASE_DIR / 'db.sqlite3')),
+        default=config('DATABASE_URL'),
         conn_max_age=600,
-        ssl_require=not DEBUG
+        ssl_require=not DEBUG  # Only enable SSL for PostgreSQL in production
     )
 }
 
