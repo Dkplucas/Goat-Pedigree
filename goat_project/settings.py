3e5f70ee4785d5f43b9992145ad960cd20b7e9dc
lu <dossoukponganfleming@gmail.com>
2025-03-30 18:50:39 +0900

    version 0.6.2

--- goat_project/settings.py
+++ goat_project/settings.py
@@ -157,7 +157,7 @@ STATICFILES_DIRS = [
 ]
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For development too
     STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
