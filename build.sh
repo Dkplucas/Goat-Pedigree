8d2962f95f2254e69a1e43918e64b60aa0b25882
lu <dossoukponganfleming@gmail.com>
2025-03-30 20:41:26 +0900

    version 0.7.6

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
+mkdir -p media/goats
+python manage.py collectstatic --noinput --clear
 pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput
