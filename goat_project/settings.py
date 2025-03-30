f24d6c436fc649bcbe3d8291c890e5e07c05e6cb
lu <dossoukponganfleming@gmail.com>
2025-03-30 21:15:48 +0900

    version 0.7.8

--- goat_project/settings.py
+++ goat_project/settings.py
@@ -154,18 +154,19 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    
 ]
+
+# For production
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     
-    # Add WhiteNoise configuration
+    # Add these WhiteNoise configurations
     WHITENOISE_MANIFEST_STRICT = False
     WHITENOISE_USE_FINDERS = True
+    WHITENOISE_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Add this line
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For development too
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Media files configuration
 MEDIA_URL = '/media/'
