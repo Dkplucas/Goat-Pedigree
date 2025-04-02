1b842c0aa43a517ebccf59bbcd1361e3ff56145c
lu <dossoukponganfleming@gmail.com>
2025-04-02 09:25:48 +0900

    version 0.03

--- goat_project/settings.py
+++ goat_project/settings.py
@@ -29,6 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = '9865d1d784fa44d2842387412a05e103' 
 DEBUG = False
 
 ALLOWED_HOSTS = ['goatpedigree.pro', 'www.goatpedigree.pro', '64.226.88.215', 'localhost', '127.0.0.1']
